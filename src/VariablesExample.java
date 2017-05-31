5c7b009901de9b14a4cc77c280c92d3f8ab5c245
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2017-05-31 20:57:04 +0300

    warnings

--- src/VariablesExample.java
+++ src/VariablesExample.java
@@ -1,15 +1,22 @@
 public class VariablesExample {
+    @SuppressWarnings("WeakerAccess")
     boolean statusOn;
+
+    @SuppressWarnings("unused")
     double javaVar = 2.34;
+
     public static void main(String[] args) {
-        int itemsSold = 04;
+        int itemsSold = 0b1101;
         double salary = 1.234e3;
         float itemCost = 11.0f;
         int i = 0xFd45, k$;
         double _interestRate;
     }
+
+    @SuppressWarnings("unused")
     public void javaMethod() {
         long simpleVar = 1_000_000_000_000L;
         byte byteVar2 = 123;
+        System.out.println(statusOn);
     }
 }
