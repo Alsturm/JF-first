b7806ca4d796819c1a8586a4405bde68cd395395
Alexander Radchenko <partner.ar@gmail.com>
2017-07-17 14:01:36 +0300

    1. Algorithms: Pow is broken. Added tests failing Pow. 2. Added PowRecurve class with proper fast power algorithm. 3. build.gradle: changed jupiter ver to 5.0.0-M4

---
+++ algorithms/src/main/java/ru/training/jf/algorithms/PowRecurve.java
@@ -0,0 +1,19 @@
+package ru.training.jf.algorithms;
+
+import java.math.BigInteger;
+
+public class PowRecurve {
+    private static BigInteger result;
+    //Get rid of recurve fork using variable temp
+    private static BigInteger temp;
+
+    public static BigInteger pow(BigInteger basis, int power) {
+        if (power == 0)
+            return new BigInteger(("1"));
+        temp = pow(basis, power >> 1);
+        result = temp.multiply(temp);
+        if ((power & 1) == 1)
+            result = result.multiply(basis);
+        return result;
+    }
+}
---
+++ algorithms/src/test/java/ru/training/jf/algorithms/PowRecurveTest.java
@@ -0,0 +1,35 @@
+package ru.training.jf.algorithms;
+
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static ru.training.jf.algorithms.PowRecurve.*;
+
+class PowRecurveTest {
+    @Test
+    void powTest() {
+        assertThat(pow(new BigInteger("0"), 0), Is.is(new BigInteger("1")));
+        assertThat(pow(new BigInteger("0"), 2), Is.is(new BigInteger("0")));
+        assertThat(pow(new BigInteger("1"), 1), Is.is(new BigInteger("1")));
+        assertThat(pow(new BigInteger("2"), 2), Is.is(new BigInteger("4")));
+        assertThat(pow(new BigInteger("3"), 2), Is.is(new BigInteger("9")));
+    }
+    @Test
+    void pow2_4() {
+        assertThat(pow(new BigInteger("2"), 4), Is.is(new BigInteger("16")));
+    }
+    @Test
+    void pow3_6() {
+        assertThat(pow(new BigInteger("3"), 6), Is.is(new BigInteger("729")));
+    }
+    @Test
+    void pow3_17() {
+        assertThat(pow(new BigInteger("3"), 17), Is.is(new BigInteger("129140163")));
+    }
+
+
+
+}
